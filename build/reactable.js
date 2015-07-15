d75756491890de626c2f0891ee299a27844db7e3
Chad <cythrawll@codeangel.org>
2015-07-15 11:56:25 -0500

    fixed bug that should have been fixed

--- build/reactable.js
+++ build/reactable.js
@@ -378,7 +378,7 @@
                             onFilter: this.props.onFilter, 
                             placeholder: this.props.filterPlaceholder, 
                             value: this.props.currentFilter}
-                        ) : '', 
+                        ) : null, 
                     React.createElement("tr", {className: "reactable-column-header"}, Ths)
                 )
             );
